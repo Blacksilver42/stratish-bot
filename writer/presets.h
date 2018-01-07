6e05ba81d3e2945f9866ba3f08b5100e2dd66762
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-01-07 21:59:24 +0000

    comments!

--- writer/presets.h
+++ writer/presets.h
@@ -3,19 +3,26 @@
 #ifndef __SBDRAW_PRESETS_H__
 #define __SBDRAW_PRESETS_H__
 
+
+// outside padding
 #define IMAGE_PADDING 15
 
+// base glyph size
 #define GLYPH_SIZE 100 //must be a multiple of 20
 
-#define SPACER 10
-#define DECO_SPACER 5
-
+// decoration sizes
 #define THIN_DECO_SIZE 40
 #define BOLD_DECO_SIZE 20
 
+// space between glyphs and decos
+#define SPACER 10
+#define DECO_SPACER 5
+
+// internal glyph multipliers
 #define MINI_GLYPH_SIZE_MULT 0.5
 #define MINI_GLYPH_OFFSET_MULT 0.15
 
+// cast to an int or it starts complaining about loss of percision
 #define MINI_GLYPH_OFFSET (int)(GLYPH_SIZE*MINI_GLYPH_OFFSET_MULT)
 #define MINI_GLYPH_SIZE (int)(GLYPH_SIZE*MINI_GLYPH_SIZE_MULT)
 
