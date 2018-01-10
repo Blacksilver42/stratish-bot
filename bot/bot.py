90024fcde19915eb5e18d3fed465cb0b135cb668
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-01-10 13:32:36 +0000

    /s grabbed a leading space; fixed.

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,7 @@ from profanity import profanity
 from os import system
 
 import helpers
+import actions
 
 IGNORE_CHITCHAT = False
 
@@ -38,67 +39,7 @@ print("PREFIX =", PREFIX)
 
 print("IGNORE_CHITCHAT =", IGNORE_CHITCHAT)
 
-async def wtf(M):
-	await client.add_reaction(M, "❓")
 
-async def nope(M):
-	await client.add_reaction(M, "🚫")
-
-async def check(M):
-	await client.add_reaction(M, "✅")
-
-
-
-async def refuse(M):
-	proc = subprocess.Popen(["/usr/games/fortune", "bofh-excuses"],
-		stdout=subprocess.PIPE)
-	(out, err) = proc.communicate()
-	out = out.decode()
-	msg = "**Error:** " + out.split("\n",2)[2];
-	await client.send_message(M.channel,msg)
-
-
-async def pull(M):
-	proc = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-	(out, err) = proc.communicate()
-	out = out.decode()
-	#print("'"+out+"'")
-	if(out != "Already up-to-date.\n"):
-		await client.send_message(M.channel, "```\n"+out+"\n```")
-		make()
-	else:
-		await check(M)
-
-
-async def make(M):
-	await client.send_typing(M.channel)
-	proc = subprocess.Popen(["bot/make.sh"], stdout=subprocess.PIPE)
-	(out, err) = proc.communicate()
-	out = out.decode()
-	await client.send_message(M.channel, "```\n"+out+"\n```")
-
-
-async def stratish(M, words):
-	if(words[0]!=' '):
-		await wtf(M)
-		return
-	
-	if(profanity.contains_profanity(words)):
-		await refuse(M)
-		try:
-			await client.delete_message(M)
-		finally:
-			return
-	
-	await client.send_typing(M.channel)
-	ut = time.strftime("%s")
-	tmpfile = "tmp/img-"+str(ut)+".png"
-	command = "writer/sbdraw '%s' '%s'" % (words, tmpfile)
-	print("$", command)
-	system("touch "+tmpfile)
-	system(command)
-	await client.send_file(M.channel, tmpfile, content="`"+words+"`")
-	
 
 
 ###############################################################################
@@ -135,7 +76,7 @@ async def on_message(M):
 		await stratish(M, M.content[9:])
 
 	if(M.content.startswith(PREFIX + 's') or M.content.startswith(PREFIX+"S")):
-		await stratish(M, M.content[2:])
+		await stratish(M, M.content[3:])
 
 	if(M.content.startswith(PREFIX + 'pull') or str(M.author) == "GitHub#0000"):
 		await pull(M)
